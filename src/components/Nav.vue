93035c603b0cea46ca744f5982047f1f848051f6
Lionyu <779625444@qq.com>
2021-03-12 19:28:51 +0800

    创建<Icon />组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="nav">
-    <router-link to="/labels">主页</router-link>
+    <router-link to="/labels">
+    <Icon name="label"/>
+      主页
+    </router-link>
     |
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+    <Icon name="money"/>
+      记账
+    </router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+
+
+
 export default {
   name: 'Nav'
 };

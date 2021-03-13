056b0590fc84f54a1b283e1c0f6dab2e1cd4356f
Lionyu <779625444@qq.com>
2021-03-13 16:28:07 +0800

    模块化Money

---
+++ src/components/Money/Types.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ul class="types">
+      <li class="selected">支出</li>
+      <li class="selected">收入</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Types'
+};
+</script>
+
+<style lang="scss" scoped>
+.types {
+  background: #0C78FF;
+  display: flex;
+  font-size: 24px;
+  text-align: center;
+
+  > li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    &.selected {
+      &::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: rgb(252, 112, 65);
+      }
+    }
+  }
+}
+</style>

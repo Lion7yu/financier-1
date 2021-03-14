75f9512074572dfbac65fbf4faf0efaa8a25c212
Lionyu <779625444@qq.com>
2021-03-14 19:04:29 +0800

    完成新建标签功能

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,14 +1,78 @@
 <template>
   <div>
     <Layout>
-      <p>Labels.vue</p>
+      <ol class="tags">
+        <li v-for="tag in tags" :key="tag">
+          <span>{{ tag }}</span>
+          <Icon name="right"/>
+        </li>
+      </ol>
+      <div class="newTag-wrapper">
+        <button class="newTag" @click="createTag">新建标签</button>
+      </div>
     </Layout>
   </div>
 </template>
 
 <script lang="ts">
 
-export default {
-  name: 'Labels',
-};
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import tagListModel from '@/models/tagListModel';
+
+tagListModel.fetch();
+@Component
+export default class Labels extends Vue {
+  tags = tagListModel.data;
+
+  createTag() {
+    const name = window.prompt('请输入标签名');
+    if (name) {
+      const message = tagListModel.create(name);
+      if (message === 'duplicated') {
+        window.alert('请勿创建重复标签');
+      }else if (message ==='success'){
+        window.alert('添加新标签成功');
+      }
+    }
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+.tags {
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+
+  > li {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e5e8;
+
+    svg {
+      width: 24px;
+      height: 24px;
+      color: #666;
+      margin-right: 16px;
+    }
+  }
+}
+
+.newTag {
+  background: #0C78FF;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+
+  &-wrapper {
+    text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
+  }
+}
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,5 @@
 <template>
   <Layout class-prefix="layout">
-    {{recordList}}
     <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
     <Types @update:value="onUpdateType"/>
     <Notes @update:value="onUpdateNotes" />
@@ -14,9 +13,11 @@ import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component,Watch} from 'vue-property-decorator';
-import model from '@/model';
+import recordListModel from '@/models/recordListModel';
+import tagListModel from '@/models/tagListModel';
 
-const recordList=model.fetch()
+const recordList=recordListModel.fetch()
+const tagList = tagListModel.fetch()
 
 
 
@@ -24,7 +25,7 @@ const recordList=model.fetch()
   components: {Tags, Notes, Types, NumberPad}
 })
 export default class  Money extends Vue{
-  tags=['衣','食','住','行','理财'];
+  tags=tagList;
   recordList: RecordItem[]=recordList;
   record: RecordItem={tags:'',notes:'',type:'-',amount:0};
   onUpdateTags(value: string){
@@ -40,14 +41,14 @@ export default class  Money extends Vue{
     this.record.amount=parseFloat(value)
   }
   saveRecord() {
-    const record2: RecordItem= model.clone(this.record)
+    const record2: RecordItem= recordListModel.clone(this.record)
     record2.createdAt = new Date();
     this.recordList.push(record2)
 
   }
   @Watch('recordList')
   onRecordListChange(){
-    model.save(this.recordList)
+    recordListModel.save(this.recordList)
   }
 }
 </script>

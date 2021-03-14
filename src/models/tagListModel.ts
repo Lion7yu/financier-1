f44289fead3219e45f65e55372ce2f92e5e445d0
Lionyu <779625444@qq.com>
2021-03-14 23:44:22 +0800

    实现可编辑和删除标签

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,5 +1,5 @@
 const localStorageKeyName = 'tagList';
-type Tag={
+type Tag = {
   id: string;
   name: string;
 }
@@ -8,6 +8,8 @@ type TagListModel = {
   fetch: () => Tag[];
   create: (name: string) => 'success' | 'duplicated'; // 联合类型
   save: () => void;
+  update: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
+  remove: (id: string) => boolean;
 }
 const tagListModel: TagListModel = {
   data: [],
@@ -17,10 +19,38 @@ const tagListModel: TagListModel = {
   },
   create(name) {
     const names = this.data.map(item => item.name);
-    if (names.indexOf(name)>=0){return 'duplicated';}
-    this.data.push({id:name,name:name});
+    if (names.indexOf(name) >= 0) {return 'duplicated';}
+    this.data.push({id: name, name: name});
     this.save();
     return 'success';
+  },
+  update(id: string, name: string) {
+    const idList = this.data.map(item => item.id);
+    if (idList.indexOf(id) >= 0) {
+      const names = this.data.map(item => item.name);
+      if (names.indexOf(name) >= 0) {
+        return 'duplicated';
+      } else {
+        const tag = this.data.filter(item => item.id === id)[0];
+        tag.name = name;
+        this.save();
+        return 'success';
+      }
+    } else {
+      return 'not found';
+    }
+  },
+  remove(id: string) {
+    let index = -1;
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+    this.data.splice(index, 1);
+    this.save();
+    return true;
   },
   save() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));

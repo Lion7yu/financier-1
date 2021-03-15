6cb4862e7912d249d66face579343546c724e0aa
Lionyu <779625444@qq.com>
2021-03-15 23:05:05 +0800

    完成store

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,28 +1,11 @@
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
- const store= {
-  //record store
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => recordListModel.create(record),
-  //tag store
-  tagList: tagListModel.fetch(),
-   findTag(id: string){
-     return this.tagList.filter(t => t.id ===id)[0];
-   },
-   createTag: (name: string) => {
-    const message = tagListModel.create(name);
-    if (message === 'duplicated') {
-      window.alert('请勿创建重复标签');
-    } else if (message === 'success') {
-      window.alert('添加新标签成功');
-    }
-  },
-  removeTag: (id: string) => {
-    return tagListModel.remove(id);
-  },
-  updateTag: (id: string, name: string) => {
-    return tagListModel.update(id, name);
-  },
+const store = {
+  ...recordStore,
+  ...tagStore,
 };
- export default store;
+
+console.log(store);
+
+export default store;

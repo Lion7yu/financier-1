8ea2159494ff5ebfec4a707a9af578d2c96bd765
Lionyu <779625444@qq.com>
2021-03-16 15:50:41 +0800

    修复了一些bug

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-    <ol>
+    <ol v-if="groupedList.length>0">
       <li v-for="(group, index) in groupedList" :key="index">
         <h3 class="title">{{ beautify(group.title) }} <span>￥{{ group.total }}</span></h3>
         <ol>
@@ -15,6 +15,9 @@
         </ol>
       </li>
     </ol>
+    <div v-else class="noResult">
+      你还没有记录哦
+    </div>
   </Layout>
 </template>
 <script lang="ts">
@@ -30,7 +33,7 @@ import clone from '@/lib/clone';
 })
 export default class Statistics extends Vue {
   tagString(tags: Tag[]) {
-    return tags.length === 0 ? '无' : tags.join(',');
+    return tags.length === 0 ? '无' : tags.map(t=>t.name).join('，');
   }
 
   beautify(string: string) {
@@ -56,7 +59,6 @@ export default class Statistics extends Vue {
 
   get groupedList() {
     const {recordList} = this;
-    if (recordList.length === 0) {return [];}
 
     const newList = clone(recordList)
         .filter(r => r.type === this.type)
@@ -95,6 +97,10 @@ export default class Statistics extends Vue {
 </script>
 
 <style scoped lang="scss">
+.noResult{
+  padding: 16px;
+  text-align: center;
+}
 ::v-deep {
   .type-tabs-item {
     background: #0C78FF;

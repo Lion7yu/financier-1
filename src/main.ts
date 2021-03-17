c136b5184e3ee52a67a0051330f3cb34e26d0d19
Lionyu <779625444@qq.com>
2021-03-17 11:30:09 +0800

    提示用户用手机访问应用

--- src/main.ts
+++ src/main.ts
@@ -19,11 +19,19 @@ new Vue({
   render: h => h(App)
 }).$mount('#app');
 
-window.onload = function (){
-  setTimeout(function (){
-    window.scrollTo(0,10000);
-  },0);
-}
-if(document.documentElement.clientWidth>500){
-  window.alert('使用手机访问页面体验更佳哦')
+window.onload = function () {
+  setTimeout(function () {
+    window.scrollTo(0, 10000);
+  }, 0);
+};
+if (document.documentElement.clientWidth > 500) {
+  window.alert('使用手机访问页面体验更佳哦');
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform='translate(-50%,-50%)';
+  img.style.boxShadow= '0 0 10px rgba(0,0,0,0.25);'
+  document.body.appendChild(img);
 }
